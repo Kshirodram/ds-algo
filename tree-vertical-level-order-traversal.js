09cd13b9c552dd61217c3f9a82956bc3f7604565
Kshirodra Meher <kshirodra.meher@walmart.com>
2020-08-10 02:43:53 +0530

    added vertical  level order traversal of a tree

--- tree-vertical-level-order-traversal.js
+++ tree-vertical-level-order-traversal.js
@@ -51,43 +51,53 @@ const test = {
 };
 
 var verticalTraversal = function (root) {
-  const result = [];
-  const queue = [];
+  // dfs or bfs but calculate the coordinates while running it
+  // using bfs we won't have to use the Y values to sort (?)
+  // left goes to X-1, Y-1, right goes to X+1, Y-1
+  let stack = [{ ...root, x: 0, y: 0 }];
 
-  if (root !== null) {
-    const hdNodes = {};
-    queue.push({ node: root, distance: 0 });
-    hdNodes[0] = [root.val];
+  let xCoordMap = {};
 
-    while (queue.length > 0) {
-      const temp = queue.shift();
-      if (temp.node.left !== null) {
-        let tempCounter = temp.distance - 1;
-        queue.push({ node: temp.node.left, distance: tempCounter });
-        if (hdNodes[tempCounter]) {
-          hdNodes[tempCounter].push(temp.node.left.val);
-        } else {
-          hdNodes[tempCounter] = [temp.node.left.val];
-        }
-      }
-      if (temp.node.right !== null) {
-        let tempCounter = temp.distance + 1;
-        queue.push({ node: temp.node.right, distance: tempCounter });
-        if (hdNodes[tempCounter]) {
-          hdNodes[tempCounter].push(temp.node.right.val);
-        } else {
-          hdNodes[tempCounter] = [temp.node.right.val];
-        }
-      }
+  const addNode = (val, x, y) => {
+    // if the key, 'x', already exists, push it
+    // if it doesn't, make a new array
+    if (xCoordMap[`${x}`]) {
+      xCoordMap[`${x}`].push({ val, y });
+    } else {
+      xCoordMap[`${x}`] = [{ val, y }];
     }
-    
-    Object.keys(hdNodes)
-      .sort((a, b) => a - b)
-      .forEach((key) => {
-        result.push(hdNodes[key]);
-      });
-    return result;
+  };
+
+  while (stack.length > 0) {
+    const currNode = stack.pop();
+
+    addNode(currNode.val, currNode.x, currNode.y);
+
+    if (currNode.left) {
+      stack.push({ ...currNode.left, x: currNode.x - 1, y: currNode.y - 1 });
+    }
+
+    if (currNode.right) {
+      stack.push({ ...currNode.right, x: currNode.x + 1, y: currNode.y - 1 });
+    }
   }
+
+  // we have an object with numbered keys and arrays of values
+  const sortedKeys = Object.keys(xCoordMap).sort(
+    (a, b) => Number(a) - Number(b)
+  );
+  const vertArrays = sortedKeys.map((key) => {
+    // sort the array then return it with only vals, not x and y
+    // sort by y first, then by value if y's are the same
+    xCoordMap[key]
+      .sort((a, b) => b.y - a.y)
+      .sort((a, b) => {
+        if (a.y === b.y) return a.val - b.val;
+        return 0;
+      });
+    return xCoordMap[key].map((obj) => obj.val);
+  });
+  return vertArrays;
 };
 
-verticalTraversal(test);
+console.log(verticalTraversal(test));
